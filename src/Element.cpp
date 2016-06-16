5637b4056fa20cbca785d921450f856617ea9070
virgilio <virgiliojose1294@gmail.com>
2016-06-16 13:30:56 -0600

    cambie algunas cosa de element,cpp y de main.cpp
    cambie algunas cosa de element,cpp y de main.cpp

--- src/Element.cpp
+++ src/Element.cpp
@@ -7,6 +7,123 @@
 
 #include "Element.h"
 
-Element::Element(std::string name):_name(name), _next(NULL)
+Lista::Lista(Libros Numero)
 {
+	PNodo q = new Nodo();
+	q->Elemento = Numero;
+	q->Proximo = this->Primero;
+	this->Primero = q;
+}
+
+int Lista::Agregar(Libros NuevoElemento)
+{
+	PNodo p = Primero;
+	PNodo q = new Nodo;
+
+	q->Elemento = NuevoElemento;
+	q->Proximo = Primero;
+	Primero = q;
+
+	return 1;
+}
+
+void Lista::getUltimo()
+{
+	PNodo p = Primero;
+	int Posicion = 0;
+	int i = 0;
+
+	while (i != Posicion && p != NULL)
+	{
+		p = p->Proximo;
+		i++;
+	}
+
+	cout << p->Elemento;
+}
+
+int Lista::Vacio()
+{
+	PNodo p = Primero;
+
+	if (Primero == NULL)
+	{
+		return 1;
+	}
+
+	else
+	{
+		return 0;
+	}
+}
+
+int Lista::getLongitud()
+{
+	int i = 0;
+
+	for (PNodo p = Primero; p != NULL; p = p->Proximo)
+	{
+		i++;
+	}
+
+	return i;
+}
+
+Libros Lista::BuscarElemento(int Position)
+{
+	PNodo p = Primero;
+
+	int i = 0;
+
+	while (i != Position && p != NULL)
+	{
+		p = p->Proximo;
+		i++;
+	}
+
+	return p->Elemento;
+}
+
+void Lista::Imprimir()
+{
+	for (PNodo p = Primero; p != NULL; p = p->Proximo)
+	{
+		cout << p->Elemento;
+	}
+}
+
+ostream & operator << (ostream & _Imprimir, Libros _Libros)
+{
+	_Imprimir << "\n * Titulo: " << _Libros.getTitulo() << endl;
+
+	_Imprimir << "\n * Autor: " << _Libros.getAutor() << endl;
+
+	_Imprimir << "\n * Editora: " << _Libros.getEditora() << endl;
+
+	_Imprimir << "\n * Fecha de publicacion: " << _Libros.getDia() << "/" << _Libros.getMes() << "/" << _Libros.getAno() << endl;
+
+	_Imprimir << "\n * Categoria: " << _Libros.getCategoria() << endl;
+
+	cout << endl;
+
+	return  _Imprimir;
+}
+
+void Lista::Buscar(string _Buscar)
+{
+	for (PNodo p = Primero; p != NULL; p = p->Proximo)
+	{
+		if (_Buscar == p->Elemento.getCategoria())
+		{
+			cout << "\n * Titulo: " << p->Elemento.getTitulo() << endl;
+
+			cout << "\n * Autor: " << p->Elemento.getAutor() << endl;
+
+			cout << "\n * Editora: " << p->Elemento.getEditora() << endl;
+
+			cout << "\n * Fecha de publicacion: " << p->Elemento.getDia() << "/" << p->Elemento.getMes() << "/" << p->Elemento.getAno() << endl;
+
+			cout << "\n * Categoria: " << p->Elemento.getCategoria() << endl;
+		}
+	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,161 @@ using namespace std;
 
 int main()
 {
-	Menu menu;
-	menu.show();
-	return EXIT_SUCCESS;
+	Lista L;
+
+	string titulo;
+	string autor;
+	string editora;
+	int dia;
+	int mes;
+	int ano;
+	string categoria;
+	string buscar;
+	int Menu;
+
+	Libros L1, L2, L3;
+
+	L1.setLibro("a", "a", "a", 1, 1, 1, "a");
+	L2.setLibro("b", "b", "b", 2, 2, 2, "b");
+	L3.setLibro("c", "c", "c", 3, 3, 3, "c");
+
+	L.Agregar(L1);
+	L.Agregar(L2);
+	L.Agregar(L3);
+
+	do
+	{
+		cout << "+ Menu" << endl;
+		cout << "\n #1 Listar libros" << endl;
+		cout << "\n #2 Insertar libro" << endl;
+		cout << "\n #3 Solicitar ultimo libro de la pila" << endl;
+		cout << "\n #4 Buscar libro por categoria" << endl;
+		cout << "\n #5 Salir" << endl;
+		cout << "\n - Elija una opcion: #";
+		cin >> Menu;
+
+		switch (Menu)
+		{
+		default:
+		{
+			cout << "\n* Opcion incorrecta" << endl;
+
+			cout << endl;
+			system("Pause");
+			system("cls");
+
+			break;
+		}
+
+		case 1:
+		{
+			cout << "\n #1 Listar libros" << endl;
+
+			L.Vacio();
+
+			L.Imprimir();
+
+			cout << endl;
+			system("Pause");
+			system("cls");
+
+			break;
+		}
+
+		case 2:
+		{
+			cout << "\n #2 Insertar libro" << endl;
+
+			cout << "\n - Digite el titulo: ";
+			cin >> titulo;
+
+			cout << "\n - Digite el autor: ";
+			cin >> autor;
+
+			cout << "\n - Digite la editora: ";
+			cin >> editora;
+
+			cout << "\n - Digite la fecha de publicacion" << endl;
+
+			cout << "\n - Digite el Dia: ";
+			cin >> dia;
+
+			cout << "\n - Digite el mes: ";
+			cin >> mes;
+
+			cout << "\n - Digite el ano: ";
+			cin >> ano;
+
+			cout << "\n - Digite la categoria: ";
+			cin >> categoria;
+
+			Libros A(titulo, autor, editora, dia, mes, ano, categoria);
+
+			L.Agregar(A);
+
+			cout << endl;
+
+			cout << "\n * Agregada" << endl;
+
+			cout << endl;
+
+			cout << endl;
+			system("Pause");
+			system("cls");
+
+			break;
+		}
+
+		case 3:
+		{
+			cout << "\n #3 Solicitar ultimo libro de la pila" << endl;
+
+			cout << endl;
+
+			cout << "\nUltimo elemento:" << endl;
+
+			L.getUltimo();
+
+			cout << endl;
+
+			cout << endl;
+			system("Pause");
+			system("cls");
+
+			break;
+		}
+
+		case 4:
+		{
+			cout << "\n #4 Buscar libro por categoria" << endl;
+
+			cout << "\n - Digite la categoria: ";
+			cin >> buscar;
+
+			L.Buscar(buscar);
+
+			cout << endl;
+			system("Pause");
+			system("cls");
+
+			break;
+		}
+
+		case 5:
+		{
+			cout << "\n #5 Salir" << endl;
+
+			cout << endl;
+			system("Pause");
+			system("cls");
+
+			return 0;
+		}
+		}
+	} while (Menu != 5);
+
+	cout << endl;
+	system("Pause");
+
+	return 0;
 }
